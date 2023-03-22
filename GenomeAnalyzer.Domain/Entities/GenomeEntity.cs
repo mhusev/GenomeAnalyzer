acf06a895f61f2c240f640aee1f1e13ded78e579
Mykola Husev <mhusev@gmail.com>
2023-03-22 18:38:20 +0200

    Implemented distribution feature

---
+++ GenomeAnalyzer.Domain/Entities/GenomeEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace GenomeAnalyzer.Domain.Entities;
+
+public class GenomeEntity
+{
+    public string Name { get; set; }
+    
+    public string RawGenome { get; set; }
+
+    public GenomeEntity(string name, string rawGenome)
+    {
+        Name = name;
+        RawGenome = rawGenome;
+    }
+}
