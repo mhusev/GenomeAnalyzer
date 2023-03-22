a1a2c44dc4bb7e4decbf8e5da4646dfeccfacaf6
Mykola Husev <mhusev@gmail.com>
2023-03-22 19:26:31 +0200

    Implemented data access layer

---
+++ GenomeAnalyzer.DAL/GenomeAnalyzer.DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GenomeAnalyzer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenomeAnalyzer.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        
+        public DbSet<GenomeEntity> Genomes { get; set; }
+    }
+}
