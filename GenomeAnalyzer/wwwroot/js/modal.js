c96667add6b166791f52222171e4895593757787
Mykola Husev <mhusev@gmail.com>
2023-04-05 16:59:19 +0300

    Modified .gitignore, added missing js files

---
+++ GenomeAnalyzer/wwwroot/js/modal.js
@@ -0,0 +1,26 @@
+﻿function openModal (parameters) {
+    const id = parameters.data;
+    const url = parameters.url;
+    const content = parameters.content;
+    
+    if (id === undefined || url === undefined) {
+        alert('Something went wrong.');
+        return;
+    }
+    
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: { "id": id },
+        success: function (response) {
+            content.find(".modal-body").html(response);
+            content.modal('show');
+        },
+        failure: function () {
+            content.modal('hide');
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+};
